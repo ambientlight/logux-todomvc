1a355b47746f895479199346a597cc355061b102
ambientlight <primary.taras.vozniuk@gmail.com>
2020-06-27 23:34:15 +0800

    patch createStore, patch dispatch with logux dispatch.sync

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -8,7 +8,7 @@ const mapStateToProps = (state, ownProps) => ({
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   setFilter: () => {
-    dispatch(setVisibilityFilter(ownProps.filter))
+    dispatch.sync(setVisibilityFilter(ownProps.filter))
   }
 })
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
+import { createLoguxCreator } from '@logux/redux'
+import { badge, badgeEn, log } from '@logux/client';
+import { badgeStyles } from '@logux/client/badge/styles';
+
+const createStore = createLoguxCreator({
+  subprotocol: '1.0.0',
+  server: process.env.NODE_ENV === 'development'
+    ? 'ws://localhost:31337'
+    : 'wss://logux.example.com',
+  userId: 'todo',  // TODO: We will fill it in next chapter
+  token: '' // TODO: We will fill it in next chapter
+});
 
 const store = createStore(reducer)
+badge(store.client, { messages: badgeEn, styles: badgeStyles });
+log(store.client);
+store.client.start();
 
 render(
   <Provider store={store}>
